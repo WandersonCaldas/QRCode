ad56b0b9fba0cbdcd9290a8a751765b523d43dc0
Wanderson Caldas <wcaldasti@gmail.com>
2020-10-19 21:36:48 -0300

    CRIAR PROJETO

---
+++ eSrc/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using MessagingToolkit.QRCode.Codec;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSrc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            String dados = "http://www.caldasti.com.br/";
+            ViewBag.Url = dados;
+
+            QRCodeEncoder qrCodecEncoder = new QRCodeEncoder();
+            qrCodecEncoder.QRCodeBackgroundColor = System.Drawing.Color.White;
+            qrCodecEncoder.QRCodeForegroundColor = System.Drawing.Color.Black;
+            qrCodecEncoder.CharacterSet = "UTF-8";
+            qrCodecEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            qrCodecEncoder.QRCodeScale = 6;
+            qrCodecEncoder.QRCodeVersion = 0;
+            qrCodecEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.Q;                                               
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (Image imageQRCode = qrCodecEncoder.Encode(dados))
+                {
+                    imageQRCode.Save(ms, ImageFormat.Png);
+                    ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }                          
+
+            return View();
+        }        
+    }
+}
